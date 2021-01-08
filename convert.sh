f98a1e4d38c377879c0be44a283b7f41358feb79
Matthias Georgi <matti.georgi@gmail.com>
2021-01-08 10:05:05 +0100

    fix script

--- convert.sh
+++ convert.sh
@@ -4,7 +4,7 @@ PKL_URL=$1
 PKL_FILE=$(basename $PKL_URL)
 PT_FILE="${PKL_FILE%.*}.pt"
 
-!wget --quiet $PKL_URL
+wget --quiet $PKL_URL
 
 python convert_weight.py --gen --disc --repo ./stylegan2 $PKL_FILE
 
