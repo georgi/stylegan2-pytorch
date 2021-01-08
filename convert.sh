5820a6cd8c2143f16f02dc8e6b2b69dac9262599
Matthias Georgi <matti.georgi@gmail.com>
2021-01-08 10:12:01 +0100

    install ninja

--- convert.sh
+++ convert.sh
@@ -4,11 +4,12 @@ PKL_URL=$1
 PKL_FILE=$(basename $PKL_URL)
 PT_FILE="${PKL_FILE%.*}.pt"
 
-wget --quiet $PKL_URL
-
-python convert_weight.py --gen --disc --repo ./stylegan2 $PKL_FILE
-
 mkdir -p /storage/models
 
-mv $PT_FILE /storage/models 
+pip install ninja
+
+wget --quiet $PKL_URL
+
+python convert_weight.py --gen --disc --repo ./stylegan2 $PKL_FILE && \
+  mv $PT_FILE /storage/models 
 
